4209a7a05eda6764c0f9d02d6a372bf0cb6e6cb7
Edsey-Omenda <omendajames0317@gmail.com>
2025-05-01 07:39:23 -0500

    Added repository patterns for mpesa intergration

---
+++ Repositories/IMpesaPaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿using JC_Ecommerce.Models.Domain;
+
+namespace JC_Ecommerce.Repositories
+{
+    public interface IMpesaPaymentRepository
+    {
+        Task SavePaymentAsync(MpesaPayment payment);
+
+        Task<MpesaPayment> GetPaymentAsync(string checkoutRequestId);
+    }
+}
---
+++ Repositories/MpesaPaymentRepository .cs
@@ -0,0 +1,27 @@
+﻿using JC_Ecommerce.Data;
+using JC_Ecommerce.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace JC_Ecommerce.Repositories
+{
+    public class MpesaPaymentRepository : IMpesaPaymentRepository
+    {
+        private readonly JCEcommerceDbContext jCEcommerceDbContext;
+
+        public MpesaPaymentRepository(JCEcommerceDbContext jCEcommerceDbContext)
+        {
+            this.jCEcommerceDbContext = jCEcommerceDbContext;
+        }
+        public async Task<MpesaPayment> GetPaymentAsync(string checkoutRequestId)
+        {
+            return await jCEcommerceDbContext.MpesaPayments.FirstOrDefaultAsync(p => p.CheckoutId == checkoutRequestId);
+        }
+
+        public async Task SavePaymentAsync(MpesaPayment payment)
+        {
+            jCEcommerceDbContext.MpesaPayments.Add(payment);    
+
+            await jCEcommerceDbContext.SaveChangesAsync();
+        }
+    }
+}
